e8c6e833e4558454a2b3a0d8af8666b6384ed27f
Karllana Cordovil <karllanacordovil@oryon.solutions>
2022-06-02 16:45:23 +0100

    1

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problem',
+        image: {
+            source: bugImageUrl,
+            alt: 'bug image'
+        },
+    },
+    IDEA: {
+        title: 'Idea',
+        image: {
+            source: ideaImageUrl,
+            alt: 'lamp image'
+        },
+    },
+    OTHER: {
+        title: 'Other',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'cloud idea image'
+        },
+    },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback() {
+        setFeedbackSent(false)
+        setFeedbackType(null);
+    }
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            { feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+            ) : (
+                <>
+                    {!feedbackType ? (
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                            <FeedbackContentStep 
+                                feedbackType={feedbackType} 
+                                onFeedbackRestartRequested={handleRestartFeedback}
+                                onFeedbackSent={() => setFeedbackSent(true)}
+                            />
+                        )}
+                </>
+            )}
+
+            <footer>
+                Builded with ♥ by <a className="underline underline-offset-2" href="https://github.com/kakacordovil/">Kaká</a> 
+            </footer>
+        </div>
+    )
+}
